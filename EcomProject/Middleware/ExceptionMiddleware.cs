b971762aa6edc5b76ce2b36a2b56c5b9031f6cff
Mohamed <robinvanhood20@gmail.com>
2025-05-14 19:49:22 +0300

    adding file of front end

--- EcomProject/Middleware/ExceptionMiddleware.cs
+++ EcomProject/Middleware/ExceptionMiddleware.cs
@@ -71,6 +71,7 @@ namespace EcomProject.Middleware
             context.Response.Headers["X-Content-Type-Options"] = "nosniff";
             context.Response.Headers["X-XSS-Protection"] = "1;mode=block";
             context.Response.Headers["X-Frame-Options"] = "DENY";
+
         }
     }
 }
